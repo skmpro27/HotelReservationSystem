f54e4616fe212e1ebb8822527bea14b58f7140d8
unknown <kumar.skm03@gmail.com>
2021-03-19 13:24:46 +0530

    added test for cheapest hotel for regular customer in HotelReservationTest.java

--- src/test/java/hotelreservation/HotelReservationTest.java
+++ src/test/java/hotelreservation/HotelReservationTest.java
@@ -181,7 +181,25 @@ public class HotelReservationTest {
         hotelReservation.setHotelTotalCost("11-Sep-2020", "12-Sep-2020");
 
         Hotel hotel = hotelReservation.cheapestHotel();
-        System.out.println("Name: " + hotel.getHotelName() + ", Total Rates: " + hotel.getTotalCost());
+        System.out.println("Name: " + hotel.getHotelName() + ", Rating: " + hotel.getRating() + ", Total Rates: " + hotel.getTotalCost());
         Assert.assertEquals("Ridgewood", hotel.getHotelName());
+    }
+
+    @Test
+    public void givenHotelRatingReturnCheapestHotelForRegularCustomerUsingStreams() throws ParseException {
+        HotelReservation hotelReservation = new HotelReservation(CustomerType.CUSTOMER_TYPE_REGULAR);
+        Hotel lakewood = new Hotel( "Lakewood", 110, 90, 3, 80, 80);
+        Hotel bridgewood = new Hotel( "Bridgewood", 150, 50, 4, 110, 50);
+        Hotel ridgewood = new Hotel( "Ridgewood", 220, 150, 5, 100,40);
+        //adding hotels
+        hotelReservation.addHotel(lakewood);
+        hotelReservation.addHotel(bridgewood);
+        hotelReservation.addHotel(ridgewood);
+
+        hotelReservation.setHotelTotalCost("11-Sep-2020", "12-Sep-2020");
+
+        Hotel hotel = hotelReservation.cheapestHotel();
+        System.out.println("Name: " + hotel.getHotelName() + ", Rating: " + hotel.getRating() + ", Total Rates: " + hotel.getTotalCost());
+        Assert.assertEquals("Bridgewood", hotel.getHotelName());
     }
 }
